807cc0b90f08e5c96d18443a2418e5e05de20ccc
Daniel Park <dpark070111@gmail.com>
2025-03-24 22:09:13 -0400

    least squares error

--- Regression/Basic2D.py
+++ Regression/Basic2D.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.linalg import inv as invert
 
+# returns [[a], [b]] where ax + b = y
 def regression_line(x_data, y_data):
     if len(x_data) != len(y_data):
         return 0
@@ -13,6 +14,17 @@ def regression_line(x_data, y_data):
     M_trans = np.matrix_transpose(M)
     return np.matmul(np.matmul(invert(np.matmul(M_trans, M)), M_trans), v)
 
+def calc_error(actual_x, actual_y, a, b):
+    expected_y = a * actual_x + b
+    return pow(actual_y - expected_y, 2)
+
+def total_error(x_data, y_data, a, b):
+    sum_error = 0
+    for i in range(len(x_data)):
+        x_point = x_data[i]
+        y_point = y_data[i]
+        sum_error += calc_error(x_point, y_point, a, b)
+    return sum_error
 
 fig, ax = plt.subplots()
 
@@ -37,6 +49,8 @@ for i in range(len(data)):
 fitted_line = regression_line(x_data, y_data)
 a = fitted_line[0][0]
 b = fitted_line[1][0]
+
+print(total_error(x_data, y_data, a, b))
 
 plt.plot(x_data, y_data, 'ro')
 plt.plot([(y_lim[0] - b)/a,(y_lim[1] - b)/a], y_lim,'b')
